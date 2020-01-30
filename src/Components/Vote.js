646b92c1999361493d836b18a77c8c3c74541979
ZulianTiger <ady0606@gmail.com>
2020-01-30 15:45:44 +0100

    Button handlers

--- src/Components/Vote.js
+++ src/Components/Vote.js
@@ -2,13 +2,21 @@ import React, { Component } from 'react'
 import { VoteContainer, Button, ButtonText } from '../Styles'
 
 export default class Vote extends Component {
+
+    voteReact() {
+        console.log('react');
+    }
+    voteAngular() {
+        console.log('angular');
+    }
+
     render() {
         return (
             <VoteContainer>
-                <Button>
+                <Button onClick={this.voteReact}>
                     <ButtonText>Vote React</ButtonText>
                 </Button>
-                <Button>
+                <Button onClick={this.voteAngular}>
                     <ButtonText>Vote Angular</ButtonText>
                 </Button>
             </VoteContainer>
